7acf39e6683579f9230665dd8c6505612101cbdb
Carlenekp <carlene.kpakpo@efrei.net>
2024-01-15 20:15:15 +0100

    update test

--- test_main.py
+++ test_main.py
@@ -1,24 +1,44 @@
+from typing import ItemsView
 import unittest
 from app import app
 
+import  jpype     
+import  asposecells     
+jpype.startJVM() 
+from asposecells.api import Workbook
+
+
 class TestFlaskApp(unittest.TestCase):
+    
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
+        self.items = []
 
     def test_read_page(self):
         # check if the page is loaded
-        pass
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200, "Response should be 200 OK")
 
     def test_add_item(self):
         # Test adding an item
         response = self.app.post('/add', data=dict(item="Test Item"), follow_redirects=True)
-        self.assertEqual(response.status_code, 200, "Response should be 200 OK")
+        print('item',response.data.decode("utf-8"))
+
+        data2 = response.data.decode("utf-8")
+        print("data", data2.item)
+        self.assertEqual(response.status_code, 302, "Response should be 302 OK")
+        self.assertEqual(len(self.items),1 )
+        self.assertEqual(self.items[0], 'Test Item')
+        self.assertIn('Test Item', self.items)
 
     def test_delete_item(self):
         # you can refer to the app by using self.app
         # make a post request with self.app.post(...
-        pass
+        self.items = ['Item', 'Item2']
+        response = self.app.post('/delete/0')
+        self.assertEqual(response.status_code, 200, "Response should be 200 OK")
+        self.assertEqual(len(self.items), 1)
         
     def test_update_item(self):
         pass
